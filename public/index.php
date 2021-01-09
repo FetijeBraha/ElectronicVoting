9a6ab76a8228bfd2a6a891f7398a7e8cb3e636f7
Marigona Ibrahimi <mi34607@ubt-uni.net>
2021-01-09 22:50:21 +0100

    some changes - twig

--- public/index.php
+++ public/index.php
@@ -1,5 +1,26 @@
 <?php
 
 require __DIR__ . '/../bootstrap/app.php';
+require __DIR__ . '/../vendor/autoload.php';
+include __DIR__ . "/vendor/twig/twig/lib/Twig/Autoloader.php";  
+
+$app = new \Slim\App;
+
+$container = $app->getContainer();
+
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig(__DIR__ . '/resources/views', [
+        'cache' => false
+    ]);
+
+    $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUrl()->getBasePath()), '/');
+    $view->addExctension(new Slim\Views\TwigExtension($container['router'], $basePath));
+    
+    return $view;
+};
+
+$app->get('/', function(){
+    //
+});
 
 $app->run();
